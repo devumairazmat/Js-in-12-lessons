424eab736e881f00b35ef545558eccf55e344acf
devumairazmat <umairazmat8260@gmail.com>
2022-07-11 02:48:17 +0500

    JS in 12 lessons

---
+++ script.js
@@ -0,0 +1,12 @@
+// Variables
+let firstName = "Umair";
+let lasttName = "Azmat";
+let fullName = firstName + " " + lasttName;
+console.log(fullName);
+
+const pi = 7.14;
+pi=8;
+// Value can' be changed with const
+console.log(pi);
+
+// strings
